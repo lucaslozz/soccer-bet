b8edf230d3f0fdb9b32c9ad44013c32fe3d8d440
lucaslozz <lucass.mark@gmail.com>
2022-12-08 08:41:06 -0300

    adiciona link forgot passaword e sign up

--- src/tela-login/Sign.js
+++ src/tela-login/Sign.js
@@ -13,17 +13,17 @@ const Sign = () => {
     <Container className="d-flex ">
       <Form className="text-center " style={{ width: '330px' }}>
         <img
-          className="mb-4"
+          className="mb-2"
           style={{ heigt: '72px', width: '72px' }}
           src="https://www.svgrepo.com/show/104000/flaming-football.svg"
           alt="Soccer bet logo"
         />
-        <h1 className="mb-3 fs-3 fw-normal">Please sign in</h1>
+        <h1 className="mb-3 fs-3 fw-normal">Sign in</h1>
         <Form.Group controlId="sign-in-email-address">
           <Form.Control
             type="email"
             size="lg"
-            placeholder="Email Address"
+            placeholder="Enter email address"
             autoComplete="username"
             className="position-relative mb-1"
           />
@@ -32,21 +32,32 @@ const Sign = () => {
           <Form.Control
             type="password"
             size="lg"
-            placeholder="Password"
+            placeholder="Enter password"
             autoComplete="current-password"
             className="position-relative mb-3"
           />
         </Form.Group>
+
         <Form.Group
-          className="d-flex justify-content-center mb-4"
+          className="d-flex justify-content-between mb-4"
           controlId="remember-me"
         >
           <Form.Check label="Remember me" />
+          <a style={{ textDecoration: 'none' }} href="/">
+            Forgot password?
+          </a>
         </Form.Group>
+
         <div className="d-grid">
-          <Button variant="primary" size="lg">
+          <Button variant="primary" size="lg" className="mb-2">
             Sign in
           </Button>
+          <p>
+            Don't have an account?{' '}
+            <a style={{ textDecoration: 'none' }} href="/">
+              Sign up
+            </a>
+          </p>
         </div>
       </Form>
     </Container>
